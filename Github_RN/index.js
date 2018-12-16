ebc2ea9d174371de6d8b632b37170810b53b4d8e
wen <93700983@qq.com>
2018-12-17 02:40:57 +0800

    Initial commit

---
+++ Github_RN/index.js
@@ -0,0 +1,8 @@
+/** @format */
+
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+// import App from './App';
+import App from './js/App';
+
+AppRegistry.registerComponent(appName, () => App);
