b1e113317cd2706dd491f96e23f402b84911a4db
wen <93700983@qq.com>
2019-01-08 23:09:58 +0800

    enter

--- Github_RN/js/action/popular/index.js
+++ Github_RN/js/action/popular/index.js
@@ -11,15 +11,15 @@ export function onLoadPopularData(storeName, url) {
 		dispatch({ type: Types.POPULAR_REFRESH, storeName: storeName });
 		let dataStore = new DataStore();
 		dataStore.fetchData(url).then((data) => {
-			// handleData(dispatch, storeName, data)
-			dispatch({
-				type: Types.LOAD_POPULAR_SUCCESS,
-				items: data && data.data&&data.data.items,
-				storeName
-			})
-		}).catch(error =>{
+			handleData(dispatch, storeName, data)
+			// dispatch({
+			// 	type: Types.LOAD_POPULAR_SUCCESS,
+			// 	items: data && data.data && data.data.items,
+			// 	storeName
+			// })
+		}).catch(error => {
 			console.log(error);
-		dispatch({ type: Types.LOAD_POPULAR_FAIL, storeName,error});
+			dispatch({ type: Types.LOAD_POPULAR_FAIL, storeName, error });
 		})
 	}
 }
@@ -27,7 +27,7 @@ export function onLoadPopularData(storeName, url) {
 function handleData(dispatch, storeName, data) {
 	dispatch({
 		type: Types.LOAD_POPULAR_SUCCESS,
-		items: data && data.data&&data.data.items,
+		items: data && data.data && data.data.items,
 		storeName
 	})
 }
